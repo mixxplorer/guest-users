92435b3ab2f92efe895837a974499fb86ae07b45
Leonard Marschke <leonard@marschke.me>
2025-07-08 15:24:57 +0200

    feat: add ghost user to shadow to prevent marking it as internal user

--- nss/src/shadow.rs
+++ nss/src/shadow.rs
@@ -21,18 +21,42 @@ fn db_to_shadow(user: &guest_users_lib::db::models::User) -> Result<Shadow, Erro
     Ok(new_shadow_user)
 }
 
+fn get_ghost_user(
+    global_settings: guest_users_lib::helper::Config,
+) -> Result<Option<Shadow>, Error> {
+    if !global_settings.enable_ghost_user {
+        return Ok(None);
+    }
+
+    Ok(Some(Shadow {
+        name: global_settings.ghost_user_gecos_username,
+        passwd: "x".to_string(),
+        last_change: 0,
+        change_min_days: 0,
+        change_max_days: 99999,
+        change_warn_days: 7,
+        change_inactive_days: -1,
+        expire_date: -1,
+        reserved: 0,
+    }))
+}
+
 pub fn get_all_entries() -> Result<Response<Vec<Shadow>>, Error> {
     let global_settings = guest_users_lib::helper::get_config()?;
     let mut db = guest_users_lib::db::DB::new(&global_settings)?;
 
     let users = db.get_users()?;
 
-    let mut passwd_users = Vec::new();
+    let mut shadow_users = Vec::new();
     for user in users.iter() {
-        passwd_users.push(db_to_shadow(user)?);
+        shadow_users.push(db_to_shadow(user)?);
     }
 
-    Ok(Response::Success(passwd_users))
+    if let Some(ghost_user) = get_ghost_user(global_settings)? {
+        shadow_users.push(ghost_user);
+    }
+
+    Ok(Response::Success(shadow_users))
 }
 
 pub fn get_entry_by_name(name: &str) -> Result<Response<Shadow>, Error> {
@@ -41,6 +65,12 @@ pub fn get_entry_by_name(name: &str) -> Result<Response<Shadow>, Error> {
 
     if let Some(user) = db.find_user_by_name(name)? {
         return Ok(Response::Success(db_to_shadow(&user)?));
+    }
+
+    if let Some(ghost_user) = get_ghost_user(global_settings)? {
+        if ghost_user.name == name {
+            return Ok(Response::Success(ghost_user));
+        }
     }
 
     Ok(Response::NotFound)
