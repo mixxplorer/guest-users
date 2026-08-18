8639f07fdc8d3a23424bf53d6e4f2bffd04ee60f
Leonard Marschke <leonard@marschke.me>
2026-08-18 17:57:24 +0200

    fix: clippy linting errors

--- cleanup-daemon/src/main.rs
+++ cleanup-daemon/src/main.rs
@@ -48,7 +48,7 @@ async fn session_end_listener() -> anyhow::Result<()> {
             } else {
                 log::info!(
                     "Skipping user {} as the user still has an active session.",
-                    &user.user_name
+                    user.user_name
                 );
             }
         }
@@ -64,7 +64,7 @@ async fn cleanup_user(
     // notify accountservice
     log::info!(
         "Notifying account service to uncache user {}",
-        &user.user_name
+        user.user_name
     );
     let accounts_server_interface =
         guest_users_lib::zbus::accounts_service::AccountsProxy::builder(system_connection)
